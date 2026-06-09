4956fde318bab756fa75eaccbd61cf95dfe39a4c
zyrox5919-tech <zyrox5919@gmail.com>
2026-06-09 16:00:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            session_name=SESSION,
+            name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
@@ -40,7 +40,6 @@ class Bot(Client):
     async def stop(self, *args):
         await super().stop()
         print("Bot stopped. Bye.")
-
 
 app = Bot()
 app.run()
